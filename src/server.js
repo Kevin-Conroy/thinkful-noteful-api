3942f08a4e8eef41c797707448bfe1777595e657
Kevin Conroy <74108431+Kevin-Conroy@users.noreply.github.com>
2021-07-14 00:58:29 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+'use strict';
+const app = require('./app');
+const { PORT, DB_URL, NODE_ENV } = require('./config');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+  searchPath: ['knex', 'public']
+});
+
+app.set('db', db);
+
+app.listen(PORT, () => {
+  if (NODE_ENV !== 'production') {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  }
+});
